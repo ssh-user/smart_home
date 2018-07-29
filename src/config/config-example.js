4d27c7bcf4b49bf1624510f35e7da1e6d4031164
Unknown <ssh.kharkiv@gmail.com>
2018-07-29 18:37:09 +0300

    update readme and fix minor bug

--- src/config/config-example.js
+++ src/config/config-example.js
@@ -1,10 +1,32 @@
-const CONFIG = {
+// config
+module.exports = {
     "session": {
         "secret": "Cats. All like cats."
     },
+
     "server": {
-        "port": 80
+        "port": {
+            "production": 80,
+            "development": 3332
+        },
+        "defaultUser": {
+            "username": "root",
+            "password": "Hi,man!"
+        }
+    },
+
+    "gsm": {
+        "adress": "/dev/ttyS1",
+        "whiteList": ["+380991234567"],
+        "baudrate": 115200
+    },
+
+    "camera": {
+        "path": "/dev/video0",
+        "bitrate": "2M",
+        "resolution": {
+            HIGH: "640x480",
+            LOW: "320x240"
+        }
     }
 };
-
-module.exports = CONFIG;
